4def129bb4cfdadf8a976b70222f2e2eb69b29c6
EJMason <eliotjunior@gmail.com>
2017-06-16 21:08:07 -0700

    [Lesson 1] Finished first algorithm problem!

--- week-1/fibonacci/fibonacci.cpp
+++ week-1/fibonacci/fibonacci.cpp
@@ -14,32 +14,42 @@
 // 5. Remove the call to test_solution, uncomment the line with a call to fibonacci_fast (and the lines reading the input),
 //    and submit it to the grader.
 
-int fibonacci_naive(int n) {
-    if (n <= 1)
-        return n;
+// int fibonacci_naive(int n) {
+//     if (n <= 1)
+//         return n;
 
-    return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
-}
+//     return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
+// }
 
 int fibonacci_fast(int n) {
-    // write your code here
 
-    return 0;
+    if (n < 2)
+        return n;
+
+    int numbers [n];
+    numbers[0] = 0;
+    numbers[1] = 1;
+
+    for (int i = 2; i <= n; i++) {
+        numbers[i] = numbers[i - 1] + numbers[i - 2];
+    }
+
+    return numbers[n];
 }
 
-void test_solution() {
-    assert(fibonacci_fast(3) == 2);
-    assert(fibonacci_fast(10) == 55);
-    for (int n = 0; n < 20; ++n)
-        assert(fibonacci_fast(n) == fibonacci_naive(n));
-}
+// void test_solution() {
+//     assert(fibonacci_fast(3) == 2);
+//     assert(fibonacci_fast(10) == 55);
+//     for (int n = 0; n < 20; ++n)
+//         assert(fibonacci_fast(n) == fibonacci_naive(n));
+// }
 
 int main() {
     int n = 0;
     std::cin >> n;
 
-    std::cout << fibonacci_naive(n) << '\n';
-    //test_solution();
-    //std::cout << fibonacci_fast(n) << '\n';
+    // std::cout << fibonacci_naive(n) << '\n';
+    // test_solution();
+    std::cout << fibonacci_fast(n) << '\n';
     return 0;
 }
