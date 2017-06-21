70f3d90fdb67aae2bb93363a6cda1e0c04d92717
EJMason <eliotjunior@gmail.com>
2017-06-21 15:45:01 -0700

    [Lesson 2] dot_product greedy solution completed in constraints

--- week-2/dot_product.cpp
+++ week-2/dot_product.cpp
@@ -4,8 +4,38 @@
 
 using std::vector;
 
-long long max_dot_product(vector<int> a, vector<int> b) {
-  // write your code here
+int partition(vector<int>& arr, int startIdx, int endIdx) {
+  double pivot = arr[startIdx];
+  int i = startIdx;
+
+  int j;
+  for(j = startIdx + 1; j < endIdx; j++) {
+    if(arr[j] >= pivot) {
+      i = i + 1;
+      std::swap(arr[i],arr[j]);
+    }
+  }
+
+  std::swap(arr[i],arr[startIdx]);
+  return i;
+}
+
+// 0 to number of items
+void quicksort(vector<int>& arr, int leftEdge, int rightEdge) {
+  int pivot;
+
+  if (leftEdge < rightEdge) {
+    pivot = partition(arr, leftEdge, rightEdge);
+
+    quicksort(arr, leftEdge, pivot);
+    quicksort(arr, pivot + 1, rightEdge);
+  }
+}
+
+long long max_dot_product(vector<int> a, vector<int> b, int len) {
+
+  quicksort(a, 0, len);
+  quicksort(b, 0, len);
   long long result = 0;
   for (size_t i = 0; i < a.size(); i++) {
     result += ((long long) a[i]) * b[i];
@@ -13,15 +43,31 @@ long long max_dot_product(vector<int> a, vector<int> b) {
   return result;
 }
 
+// commented out main
+
 int main() {
   size_t n;
   std::cin >> n;
   vector<int> a(n), b(n);
+
   for (size_t i = 0; i < n; i++) {
     std::cin >> a[i];
   }
   for (size_t i = 0; i < n; i++) {
     std::cin >> b[i];
   }
-  std::cout << max_dot_product(a, b) << std::endl;
+
+  std::cout << max_dot_product(a, b, n) << std::endl;
+  return 0;
 }
+
+
+//int main() {
+//  vector<int> test = {5,88,3,123,5,89,6,-8};
+//  quicksort(test, 0, test.size());
+//  std::cout << "-------- Test Begin --------" << "\n";
+//  for(int i = 0; i < test.size(); ++i) {
+//    std::cout << test[i] << " ";
+//  }
+//  return 0;
+//}
