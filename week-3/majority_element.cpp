bf7bd88a962cf702d209de0edafabf82bf1b88e1
EJMason <eliotjunior@gmail.com>
2017-06-28 17:20:48 -0700

    [week-3] hopefullly this works for majority_element :)

--- week-3/majority_element.cpp
+++ week-3/majority_element.cpp
@@ -3,44 +3,64 @@
 #include <vector>
 
 int get_majority_element(std::vector<int> &a, int left, int right);
-int prune(std::vector<int> &arr);
+int linear_majority(std::vector<int> &a);
+int prune(std::vector<int> arr);
 
 int main() {
-//  int n;
-//  std::cin >> n;
-//  std::vector<int> a(n);
-//  for (size_t i = 0; i < a.size(); ++i) {
-//    std::cin >> a[i];
-//  }
+  int n;
+  std::cin >> n;
+  std::vector<int> a(n);
+  for (size_t i = 0; i < a.size(); ++i) {
+    std::cin >> a[i];
+  }
 
 //  int maj = get_majority_element(a, 0, (a.size() - 1));
-  int maj = prune(a);
+  int maj = linear_majority(a);
   std::cout << maj << '\n';
-//  std::cout << (maj != -1) << '\n';
+  std::cout << (maj != -1) << '\n';
 }
 
-int linear_majority_element(std::vector<int> &a) {
+int linear_majority(std::vector<int> &a) {
+  int x = prune(a);
+  int len = a.size();
 
+  std::cout << "The maj is: " << x << std::endl;
+  int count = 0;
+  std::cout << "The length is :" << a.size() << std::endl;
+  for (int i = 0; i < len; i++) {
+    std::cout << "i: " << i << std::endl;
+    if (a[i] == x && ++count == len/2+1 ) {
+      std::cout << "The count made it " << count<< std::endl;
+      return x;
+    }
+    std::cout << "The count: " << count << "/" << len/2+1 << std::endl;
+  }
+  std::cout << "The count was short: " << count << "/" << len/2+1 << std::endl;
+  return -1;
 }
 
-int prune(std::vector<int> &arr) {
+int prune(std::vector<int> arr) {
   int len = arr.size();
-//  std::cout << "The size is " << len << std::endl;
+
   if (len == 0) return -1;
   if (len == 1) return arr[0];
-
   if (len % 2 == 1) {
-//    std::cout << "It is odd length  " << len << std::endl;
-    int count = 1;
-    int target = len / 2 + 1;
     int value = arr.back();
     arr.pop_back();
-
-    for (int i = 0; i < len - 1; i++) {
-      if (arr[i] == value && ++count == target) return value;
-    }
-    len -= 1;
+    return value;
   }
+
+//  if (len % 2 == 1) {
+//    int count = 1;
+//    int target = len / 2 + 1;
+//    int value = arr.back();
+//    arr.pop_back();
+//
+//    for (int i = 0; i < len - 1; i++) {
+//      if (arr[i] == value && ++count == target) return value;
+//    }
+//    len -= 1;
+//  }
 
   std::vector<int> majors;
 
@@ -50,6 +70,8 @@ int prune(std::vector<int> &arr) {
   }
   return prune(majors);
 }
+
+
 
 int get_majority_element(std::vector<int> &a, int left, int right) {
   if (left == right) return -1;
