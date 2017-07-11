b90248b4c6fcbcd95bdbe535cf3c7ca01f81878b
EJMason <eliotjunior@gmail.com>
2017-07-11 16:49:46 -0700

    [lesson-4] First attempt at knapsack

--- week-4/knapsack.cpp
+++ week-4/knapsack.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using std::vector;
 
-int optimal_weight(int W, const vector<int> &w) {
+int optimal_weight(int W, const std::vector<int> &w) {
   //write your code here
   int current_weight = 0;
   for (size_t i = 0; i < w.size(); ++i) {
@@ -14,12 +14,50 @@ int optimal_weight(int W, const vector<int> &w) {
   return current_weight;
 }
 
+int optimalGold(int W, const std::vector<int> &values) {
+  //first we need to create 2 arrays
+  std::vector<int> *curr = new std::vector<int> (W+1,0);
+  std::vector<int> *prev = new std::vector<int> (W+1);
+  std::vector<int> *temp;
+  int newPossibleBest, prevBest, currentValue, i, solution;
+  //need to iterate all possible weights
+  for(int value_idx = 0; value_idx < values.size(); value_idx++) {
+    temp = curr;
+    curr = prev;
+    prev = temp;
+
+    currentValue = values[value_idx];
+    if(currentValue > W) continue;
+
+    i = -1;
+    while(++i < currentValue)
+      (*curr)[i] = prev->at(i);
+
+    for(i; i <= W; i++) {
+      newPossibleBest = currentValue + prev->at(i-currentValue);
+      prevBest = prev->at(i);
+
+      curr->at(i) = std::max(newPossibleBest, prevBest);
+    }
+  }
+  solution = curr->back();
+
+  temp = NULL;
+  curr = NULL;
+  prev = NULL;
+  delete temp;
+  delete curr;
+  delete prev;
+
+  return solution;
+}
+
 int main() {
   int n, W;
   std::cin >> W >> n;
-  vector<int> w(n);
+  std::vector<int> w(n);
   for (int i = 0; i < n; i++) {
     std::cin >> w[i];
   }
-  std::cout << optimal_weight(W, w) << '\n';
+  std::cout << optimalGold(W, w) << '\n';
 }
--- week-4/main.cpp
+++ week-4/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::vector<int> *prev = new std::vector<int> (10,0);
+  std::vector<int> *prev2 = new std::vector<int> (10,0);
+  (*prev)[2] = 5;
+  prev->at(3) = 599;
+  (*prev2)[2] = 666;
+
+//  int a = -1;
+//
+//  while(++a < 10) {
+//    std::cout << a << " ";
+//    std::cout << " :D ";
+//  }
+
+  std::cout << "\nCheck it --> " << prev->at(3) << std::endl;
   return 0;
 }
