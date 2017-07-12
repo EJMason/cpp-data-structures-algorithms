0d8efe9ccb0465b854037a7993662c0d5088507d
EJMason <eliotjunior@gmail.com>
2017-07-11 18:54:52 -0700

    finished knapsack

--- week-4/knapsack.cpp
+++ week-4/knapsack.cpp
@@ -3,61 +3,24 @@
 #include <cassert>
 #include <algorithm>
 
-
-int optimal_weight(int W, const std::vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
-    }
-  }
-  return current_weight;
-}
-
-int weight(int W, std::vector<int> &m) {
-
-  int current_weight = 0;
-  int temp;
-  for (int i = 0; i < m.size(); i++) {
-    for (int j = i + 1; j < m.size(); j++) {
-      if (m[i] < m[j]) {
-        temp = m[j];
-        m[j] = m[i];
-        m[i] = temp;
-      }
-    }
-  }
-
-  for (size_t i = 0; i < m.size(); ++i) {
-
-    if (current_weight + m[i] <= W) {
-      current_weight += m[i];
-    }
-  }
-  return current_weight;
-}
-
 int optimalGold(int W, const std::vector<int> &values) {
-  //first we need to create 2 arrays
   std::vector<int> *curr = new std::vector<int> (W+1,0);
   std::vector<int> *prev = new std::vector<int> (W+1);
   std::vector<int> *temp;
   int newPossibleBest, prevBest, currentValue, i, solution;
-  //need to iterate all possible weights
+
   for(int value_idx = 0; value_idx < values.size(); value_idx++) {
+    currentValue = values[value_idx];
+    if(currentValue > W) continue;
+
     temp = curr;
     curr = prev;
     prev = temp;
-
-    currentValue = values[value_idx];
-    if(currentValue > W) continue;
 
     i = -1;
     while(++i < currentValue)
       (*curr)[i] = prev->at(i);
 
-    std::cout << "GO " << currentValue << std::endl;
     for(i; i <= W; i++) {
 
       newPossibleBest = currentValue + prev->at(i-currentValue);
@@ -66,20 +29,9 @@ int optimalGold(int W, const std::vector<int> &values) {
       curr->at(i) = std::max(newPossibleBest, prevBest);
     }
 
-    std::cout << "Prev: ";
-    for(int j = 0; j < prev->size(); j++){
-      std::cout << prev->at(j) << " ";
-    }
-    std::cout << "\n";
-
-    std::cout << "Curr: ";
-    for(int j = 0; j < curr->size(); j++){
-      std::cout << curr->at(j) << " ";
-    }
-    std::cout << "\n";
   }
 
-  solution = prev->back();
+  solution = curr->back();
 
   temp = NULL;
   curr = NULL;
@@ -91,35 +43,6 @@ int optimalGold(int W, const std::vector<int> &values) {
   return solution;
 }
 
-//void test_solution() {
-//  int randomBagTotal;
-//  int randomElementsTotal;
-//  int mine, greedy;
-//  std::vector<int> randomElements;
-//
-//  while (true) {
-//    randomBagTotal = 1 + (rand() % 50);
-//    randomElementsTotal = 3 + (rand() % 8);
-//
-//    std::cout << "Bag Size: " << randomBagTotal << "  Values: " << std::endl;
-//    std::cout << "[ ";
-//    for(int i = 0; i <=randomElementsTotal; i++) {
-//      randomElements.push_back(1 + (rand() % 30));
-//      std::cout << randomElements.back() << " ";
-//    }
-//    std::cout << "]\n" << std::endl;
-//
-//
-//    mine = optimalGold(randomBagTotal, randomElements);
-//    greedy = weight(randomBagTotal, randomElements);
-//
-//    std::cout << mine << " | " << greedy << std::endl;
-//    assert(mine == greedy);
-//    std::cout << std::endl;
-//
-//    randomElements = std::vector<int>();
-//  }
-//}
 
 //int main() {
 //  int n, W;
@@ -130,6 +53,144 @@ int optimalGold(int W, const std::vector<int> &values) {
 //  }
 //  std::cout << optimalGold(W, w) << '\n';
 //}
+
+// ================================================================================
+//                    TESTING
+// ================================================================================
+//int optimalGold(int W, const std::vector<int> &values) {
+//  //first we need to create 2 arrays
+//  std::vector<int> *curr = new std::vector<int> (W+1,0);
+//  std::vector<int> *prev = new std::vector<int> (W+1);
+//  std::vector<int> *temp;
+//  int newPossibleBest, prevBest, currentValue, i, solution;
+//  //need to iterate all possible weights
+//  for(int value_idx = 0; value_idx < values.size(); value_idx++) {
+//
+//    currentValue = values[value_idx];
+//    if(currentValue > W) continue;
+//
+//    temp = curr;
+//    curr = prev;
+//    prev = temp;
+//
+//    std::cout << "---------- " << value_idx << " -------------------" << std::endl;
+//    std::cout << "Prev: ";
+//    for(int j = 0; j < prev->size(); j++){
+//      std::cout << prev->at(j) << " ";
+//    }
+//    std::cout << "\n";
+//
+//    std::cout << "Curr: ";
+//    for(int j = 0; j < curr->size(); j++){
+//      std::cout << curr->at(j) << " ";
+//    }
+//    std::cout << "\n";
+//    std::cout << "-----------------------------" << std::endl;
+//
+//    i = -1;
+//    while(++i < currentValue)
+//      (*curr)[i] = prev->at(i);
+//
+//    std::cout << "GO " << currentValue << std::endl;
+//    for(i; i <= W; i++) {
+//
+//      newPossibleBest = currentValue + prev->at(i-currentValue);
+//      prevBest = prev->at(i);
+//
+//      curr->at(i) = std::max(newPossibleBest, prevBest);
+//    }
+//
+////    std::cout << "Prev: ";
+////    for(int j = 0; j < prev->size(); j++){
+////      std::cout << prev->at(j) << " ";
+////    }
+////    std::cout << "\n";
+////
+////    std::cout << "Curr: ";
+////    for(int j = 0; j < curr->size(); j++){
+////      std::cout << curr->at(j) << " ";
+////    }
+////    std::cout << "\n";
+//  }
+//
+//    std::cout << "THIS IS THE END:   " << std::endl;
+//    std::cout << "Prev: ";
+//    for(int j = 0; j < prev->size(); j++){
+//      std::cout << prev->at(j) << " ";
+//    }
+//    std::cout << "\n";
+//
+//    std::cout << "Curr: ";
+//    for(int j = 0; j < curr->size(); j++){
+//      std::cout << curr->at(j) << " ";
+//    }
+//    std::cout << "\n";
+//
+//  solution = curr->back();
+//
+//  temp = NULL;
+//  curr = NULL;
+//  prev = NULL;
+//  delete temp;
+//  delete curr;
+//  delete prev;
+//
+//  return solution;
+//}
+
+int max(int a, int b) { return (a > b)? a : b; }
+
+int knapSack(int W, std::vector<int> &wt, std::vector<int> &val, int n) {
+  int i, w;
+  int K[n+1][W+1];
+
+  // Build table K[][] in bottom up manner
+  for (i = 0; i <= n; i++)
+  {
+    for (w = 0; w <= W; w++)
+    {
+      if (i==0 || w==0)
+        K[i][w] = 0;
+      else if (wt[i-1] <= w)
+        K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
+      else
+        K[i][w] = K[i-1][w];
+    }
+  }
+
+  return K[n][W];
+}
+
+
+void test_solution() {
+  int randomBagTotal;
+  int randomElementsTotal;
+  int mine, greedy;
+  std::vector<int> randomElements;
+
+  while (true) {
+    randomBagTotal = 100 + (rand() % 500);
+    randomElementsTotal = 30 + (rand() % 82);
+
+    std::cout << "Bag Size: " << randomBagTotal << "  Values: " << std::endl;
+    std::cout << "[ ";
+    for(int i = 0; i <=randomElementsTotal; i++) {
+      randomElements.push_back(100 + (rand() % 250));
+      std::cout << randomElements.back() << " ";
+    }
+    std::cout << "]\n" << std::endl;
+
+
+    mine = optimalGold(randomBagTotal, randomElements);
+    greedy = knapSack(randomBagTotal, randomElements, randomElements, randomElementsTotal);
+
+    std::cout << mine << " | " << greedy << std::endl;
+    assert(mine == greedy);
+    std::cout << std::endl;
+
+    randomElements = std::vector<int>();
+  }
+}
 
 int main() {
   test_solution();
